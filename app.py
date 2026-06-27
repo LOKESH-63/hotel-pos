497fc5dcf584c16e91aebb079ef9f667c1f36d93
D lokesh reddy <dlokeshreddy3074@gmail.com>
2026-06-27 16:13:30 +0530

    Initialize Hotel POS application with Streamlit

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hotel POS",
+    page_icon="🏨",
+    layout="wide"
+)
+
+st.title("🏨 Terralogic Hotel POS")
+
+st.markdown("""
+## Welcome
+
+This Hotel POS has four modules:
+
+- 👨‍🍳 Waiter
+- 🍳 Kitchen
+- 💳 Cashier
+- 📊 Manager
+""")
+
+st.info("Use the left sidebar to open each module.")
